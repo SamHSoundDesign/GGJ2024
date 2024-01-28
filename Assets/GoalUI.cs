890c66ffbe34e5b4d2da3547a0b3b4e34f932688
SamH <66333568+SamHSoundDesign@users.noreply.github.com>
2024-01-28 22:03:27 +0000

    things

---
+++ Assets/GoalUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GoalUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI tmp;
+    private string defaultText;
+    void Start()
+    {
+        GameManager.Instance.startLevel += UpdateUI;
+        defaultText = tmp.text;
+    }
+
+    public void UpdateUI()
+    {
+        tmp.color = GameManager.Instance.currentLevelData.colorSchemeSO.colorB;
+        tmp.text = defaultText + GameManager.Instance.currentLevelData.points;
+    }
+}
